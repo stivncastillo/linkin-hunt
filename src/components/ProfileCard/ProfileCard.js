dde00f5fde3d7274689f6cffda13b6b29cce25af
Stiven <stivencastillo.90@gmail.com>
2018-02-27 01:01:24 -0500

    Some fixes for profile card

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -5,20 +5,19 @@ class ProfileCard extends Component {
         return (
             <div className="card profile">
                 <div className="card-body">
-                <div className="profile-image">
-                    <img src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/user_male2-512.png" className="img-fluid rounded-circle" alt="Profile" />
-                </div>
+                    <div className="profile-image">
+                        <img src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/user_male2-512.png" className="img-fluid rounded-circle" alt="Profile" />
+                    </div>
 
-                <div className="profile-info col-md-12">
-                    <h4>Stiven Castillo</h4>
-                    <p>stiven@stiven.com</p>
-                </div>
+                    <div className="profile-info col-md-12">
+                        <h4>Stiven Castillo</h4>
+                        <p>stiven@stiven.com</p>
+                    </div>
 
-                <div className="profile-actions">
-                    <a href="#" class="card-link">Profile</a>
-                    <a href="#" class="card-link">My Links</a>
-                    <a href="#" class="card-link">Logout</a>
-                </div>
+                    <div className="profile-actions d-flex justify-content-center">
+                        <a href="#" class="card-link">Profile</a>
+                        <a href="#" class="card-link">Logout</a>
+                    </div>
                 </div>
             </div>
         );
