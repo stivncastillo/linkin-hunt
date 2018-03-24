701262edf9d95353efdb3b126af42dc853330cf4
Stiven <stivencastillo.90@gmail.com>
2018-03-24 01:12:38 -0500

    Fill category list in the lateral menu

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
 
+import { getCategoriesFetch } from '../../actions/categoriesAction';
+
+import { connect } from 'react-redux';
+
 class CategoryList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            options: []
+        };
+    }
+
+    componentWillMount() {
+        this.props.getCategoriesFetch();
+    }
+
+    componentWillReceiveProps(nextProps){
+        this._renderFetch(nextProps);
+    }
+
+	_renderFetch = (data) => {
+        const optionsCategories = data.categoriesReducer.categories.map(option =>
+            <li key={ option._id }><a href="">{ option.name }</a></li>
+        );
+
+        this.setState({
+            options: optionsCategories
+        });
+    }
+
   render() {
     return (
         <nav className="menu">
             <h4>Topics</h4>
             <ol className="list-unstyled">
-                <li><a href="">Libraries</a></li>
-                <li><a href="">Frontend</a></li>
-                <li><a href="">Backend</a></li>
-                <li><a href="">UI</a></li>
-                <li><a href="">Frameworks</a></li>
-                <li><a href="">Toturials</a></li>
+                { this.state.options }
             </ol>
         </nav>
     );
   }
 }
 
-export default CategoryList;
+const mapStateToProps = state => {
+    return {
+        categoriesReducer: state.categoriesReducer
+    }
+};
+
+const mapDispatchToProps = dispatch =>{
+    return {
+        getCategoriesFetch: () => dispatch(getCategoriesFetch())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList)
