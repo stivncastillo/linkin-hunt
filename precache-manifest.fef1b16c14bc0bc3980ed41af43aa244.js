d2e5b15adff5e108c13bf0c88228572c54076326
Stiven <stivencastillo.90@gmail.com>
2018-11-10 01:08:11 -0500

    Updates

---
+++ precache-manifest.fef1b16c14bc0bc3980ed41af43aa244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2524f6a14955817ac87",
+    "url": "/linkin-hunt/static/js/runtime~main.d2524f6a.js"
+  },
+  {
+    "revision": "4fe85c1f1700a11369c9",
+    "url": "/linkin-hunt/static/js/main.4fe85c1f.chunk.js"
+  },
+  {
+    "revision": "44c912a4b51c329e3fef",
+    "url": "/linkin-hunt/static/js/1.44c912a4.chunk.js"
+  },
+  {
+    "revision": "4fe85c1f1700a11369c9",
+    "url": "/linkin-hunt/static/css/main.081be90d.chunk.css"
+  },
+  {
+    "revision": "e0756c07dac749cf07bd8e12a1dab1ff",
+    "url": "/linkin-hunt/index.html"
+  }
+];
